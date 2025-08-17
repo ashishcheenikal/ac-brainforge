9c2a0411ebff64609fc7ada0daaad7f681616f06
ashishcheenikal <ashishcheenikal@gmail.com>
2025-08-17 19:46:49 +0530

    27-remove-element-Array-E

---
+++ cpp/array/easy/27-remove-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> RemoveElement(vector<int> arr, int val) {
+  int j = 0;
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] != val) {
+      arr[j] = arr[i];
+      j++;
+    }
+  }
+  return arr;
+}
+
+int main() {
+
+  vector<int> sample_input_arr = {0, 10, 2, 3, 4, 5, 6, 2, 4, 2, 5, 2, 5, 2};
+  int val = 2;
+  vector<int> result_arr = RemoveElement(sample_input_arr, val);
+
+  for (int it : result_arr) {
+    cout << it;
+  }
+}
