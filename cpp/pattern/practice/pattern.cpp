84d6c685fc0a221d2eece401fe703fbcf3020225
ashishcheenikal <ashishcheenikal@gmail.com>
2025-08-28 02:07:26 +0530

    pattern-problems

--- cpp/pattern/practice/pattern.cpp
+++ cpp/pattern/practice/pattern.cpp
@@ -1,3 +1,6 @@
+// https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+// PatternProblems
+
 #include <iostream>
 using namespace std;
 
@@ -15,12 +18,11 @@ void SquarePattern(int n) {
   }
 }
 
-
-// *  
-// * *  
-// * * *  
-// * * * *  
-// * * * * * 
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
 void RightAngleTrianglePattern(int n) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= i; j++) {
@@ -30,19 +32,18 @@ void RightAngleTrianglePattern(int n) {
   }
 }
 
-
 // 1
 // 12
 // 123
 // 1234
 // 12345
-void RightAngleTriangleNumberPattern(int n){
-    for (int i =1; i <= n; i++){
-        for (int j = 1; j<= i; j++){
-            cout<< j;
-        }
-        cout << endl;
+void RightAngleTriangleNumberPattern(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j;
     }
+    cout << endl;
+  }
 }
 
 // 1
@@ -50,51 +51,261 @@ void RightAngleTriangleNumberPattern(int n){
 // 333
 // 4444
 // 55555
-void RightAngleTriangleNumberPattern1(int n){
-    for (int i =1; i <= n; i++){
-        for (int j = 1; j<= i; j++){
-            cout<< i;
-        }
-        cout << endl;
+void RightAngleTriangleNumberPattern1(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << i;
     }
-} 
+    cout << endl;
+  }
+}
 
 // *****
 // ****
 // ***
 // **
 // *
-void InvertedRightAngleTrianglePattern(int n){
-    for (int i =n; i > 0; i--){
-        for (int j = i; j > 0; j--){
-            cout<< '*';
-        }
-        cout << endl;
-      }
+void InvertedRightAngleTrianglePattern(int n) {
+  for (int i = n; i > 0; i--) {
+    for (int j = i; j > 0; j--) {
+      cout << '*';
+    }
+    cout << endl;
+  }
 };
-
 
 // 12345
 // 1234
 // 123
 // 12
 // 1
-void InvertedRightAngleTriangleNumberPattern(int n){
-    for (int i =n; i > 0; i--){
-        for (int j = 1; j <= i; j++){
-            cout<< j;
-        }
-        cout << endl;
-      }
+void InvertedRightAngleTriangleNumberPattern(int n) {
+  for (int i = n; i > 0; i--) {
+    for (int j = 1; j <= i; j++) {
+      cout << j;
+    }
+    cout << endl;
+  }
 };
 
+// - - - - *                   // (2 * i +1)
+// - - - * * *                 // (n - i)
+// - - * * * * *
+// - * * * * * * *
+// * * * * * * * * *
+void HalfDiamondPattern(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < (n - i - 1); j++) {
+      cout << ' ' << ' ';
+    }
+    for (int j = 1; j <= (2 * i + 1); j++) {
+      cout << '*' << ' ';
+    }
+    cout << endl;
+  }
+}
 
+// * * * * * * * * *
+// - * * * * * * *
+// - - * * * * *
+// - - - * * *
+// - - - - *
+void InvertedHalfDiamondPattern(int n) {
+  for (int i = n; i > 0; i--) {
+    for (int j = 1; j < (n - i + 1); j++) {
+      cout << ' ' << ' ';
+    }
+    for (int j = 1; j <= (2 * i - 1); j++) {
+      cout << '*' << ' ';
+    }
+    cout << endl;
+  }
+}
+
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+void DiamondPattern(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j < (n - i); j++) {
+      cout << ' ' << ' ';
+    }
+    for (int j = 1; j <= (2 * i + 1); j++) {
+      cout << '*' << ' ';
+    }
+    cout << endl;
+  }
+  for (int i = n; i > 0; i--) {
+    for (int j = 0; j < (n - i); j++) {
+      cout << ' ' << ' ';
+    }
+    for (int j = 0; j < (2 * i - 1); j++) {
+      cout << '*' << ' ';
+    }
+    cout << endl;
+  }
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+void VerticalHalfDiamondPattern(int n) {
+  for (int i = 0; i < (2 * n); i++) {
+    if (i < n) {
+      for (int j = 0; j < i; j++) {
+        cout << '*' << ' ';
+      }
+      cout << endl;
+    } else {
+      for (int j = 0; j < (2 * n - i); j++) {
+        cout << '*' << ' ';
+      }
+      cout << endl;
+    }
+  }
+}
+
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+// KEY start = 1 - start (which will helps toggle between 1 and 0. to find the
+// start use modulus op to find whether its odd or even)
+void Pattern11(int n) {
+  int start = 1;
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0)
+      start = 1;
+    else
+      start = 0;
+    for (int j = 0; j <= i; j++) {
+      cout << start << ' ';
+      start = 1 - start;
+    }
+    cout << endl;
+  }
+}
+
+// 1 - - - - - - - - 1
+// 1 2 - - - - - - 2 1
+// 1 2 3 - - - - 3 2 1
+// 1 2 3 4 - - 4 3 2 1
+// 1 2 3 4 5 5 4 3 2 1
+void Pattern12(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j << ' ';
+    }
+    for (int j = 1; j < 2 * (n - i) + 1; j++) {
+      cout << '-' << ' ';
+    }
+    for (int j = i; j >= 1; j--) {
+      cout << j << ' ';
+    }
+    cout << endl;
+  }
+}
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+// KEY - Set counter initial value as 1, then increment it in each loop.
+void Pattern13(int n) {
+  int count = 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << count << ' ';
+      count++;
+    }
+    cout << endl;
+  }
+}
+
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+// KEY - Take ASCII value then static_cast<char> static cast into char and
+// increment the ASCII code by 1.
+void Pattern14(int n) {
+  for (int i = 0; i < n; i++) {
+    char val = 'A';
+    int asciiCode = int(val);
+    for (int j = 0; j <= i; j++) {
+      cout << static_cast<char>(asciiCode) << ' ';
+      asciiCode++;
+    }
+    cout << endl;
+  }
+}
+
+// A B C D E
+// A B C D
+// A B C
+// A B
+// A
+void Pattern15(int n) {
+  for (int i = n; i > 0; i--) {
+    char val = 'A';
+    int asciiCode = int(val);
+    for (int j = 0; j < i; j++) {
+      cout << static_cast<char>(asciiCode) << ' ';
+      asciiCode++;
+    }
+    cout << endl;
+  }
+}
+
+
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E
+void Pattern16(int n) {
+  char val = 'A';
+  int asciiCode = int(val);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << static_cast<char>(asciiCode) << ' ';
+    }
+    asciiCode++;
+    cout << endl;
+  }
+}
 
 int main() {
-    //  SquarePattern(5);
-    //  RightAngleTrianglePattern(5);
-    // RightAngleTriangleNumberPattern(5);
-    // RightAngleTriangleNumberPattern1(5);
-    // InvertedRightAngleTrianglePattern(5);
-    InvertedRightAngleTriangleNumberPattern(5);
-  }
+  //  SquarePattern(5);
+  //  RightAngleTrianglePattern(5);
+  // RightAngleTriangleNumberPattern(5);
+  // RightAngleTriangleNumberPattern1(5);
+  // InvertedRightAngleTrianglePattern(5);
+  // // InvertedRightAngleTriangleNumberPattern(5);
+  // HalfDiamondPattern(5);
+  // InvertedHalfDiamondPattern(5);
+  // DiamondPattern(5);
+  // VerticalHalfDiamondPattern(5);
+  // Pattern11(5);
+  // Pattern12(5);
+  // Pattern13(5);
+  // Pattern14(5);
+  // Pattern15(5);
+  Pattern16(5);
+}
