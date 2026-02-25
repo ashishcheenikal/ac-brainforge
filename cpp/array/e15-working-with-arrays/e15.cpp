8f6370705c3a82e9f0df9f5082515868c6fb7c4d
ashishcheenikal <ashishcheenikal@gmail.com>
2026-02-26 02:24:20 +0530

    e15: reverse array

---
+++ cpp/array/e15-working-with-arrays/e15.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void swap(int a, int b, int arr[]) {
+  int temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+int main() {
+  int arr[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  //   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int n = size(arr);
+  cout << n << " test " << endl;
+
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < n / 2; i++) {
+    cout << arr[i] << " - " << arr[n - i - 1] << endl;
+    swap(i, (n - i - 1), arr);
+    // int temp = arr[i];
+    // arr[i] = arr[n - i - 1];
+    // arr[n - i - 1] = temp;
+  }
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
