97439b249722662ea1caeb990343dd2e7cc2113f
ashishcheenikal <ashishcheenikal@gmail.com>
2026-06-10 01:22:45 +0530

    test-codeforce-ICPC Assiut University Community

---
+++ cpp/codeforce/B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int INT;
+    long long LONG_LONG;
+    char CHAR;
+    float FLOAT;
+    double DOUBLE;
+    cin >> INT >> LONG_LONG >> CHAR >> FLOAT >> DOUBLE;
+
+    cout << INT << endl;
+    cout << LONG_LONG << endl;
+    cout << CHAR << endl;
+    cout << FLOAT << endl;
+    cout << DOUBLE << endl;
+}
---
+++ cpp/codeforce/digitsSummation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    int c = a % 10 + b % 10;
+    cout << c << endl;
+}
